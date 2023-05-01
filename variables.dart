90fe7ef77063b3a24270cf98472f14692d029383
Purna Lungeli <purnalungeli@Purnas-MacBook-Air.local>
2023-05-01 17:18:21 +0545

    1 may 2023

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  ///1. Declare a variable named myNumber and assign it a value of 42.
+  ///Print the value of the variable.
+  var myNumber = 42;
+  print(myNumber);
+
+  /// 2. Declare a variable named myString and assign it a value of "Hello, world!".
+  /// Print the value of the variable.
+  var myString = "Hello, world!";
+  print(myString);
+
+  /// 3. Declare a variable named myBoolean and assign it a value of true.
+  /// Print the value of the variable.
+  var myBoolean = true;
+  print(myBoolean);
+}
