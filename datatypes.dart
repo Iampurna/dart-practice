5e9985aa40f8c0c4dc6f1d8ca5b30f1766eaf9df
Purna Lungeli <purnalungeli@Purnas-MacBook-Air.local>
2023-05-02 17:09:36 +0545

    assignmets

---
+++ datatypes.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  //1. Declare a variable named myNumber and assign it a number value.
+  //Print the data type of the variable.
+  var myNumber = 50.000;
+  print(myNumber.runtimeType);
+
+  /// 2. Declare a variable named myString and assign it a string value.
+  /// Print the data type of the variable.
+  var myString = 'Lungeli';
+  print(myString.runtimeType);
+
+  ///3. Declare a variable named myBoolean and assign it a boolean value.
+  ///Print the data type of the variable.
+  var myBoolean = false;
+  print(myBoolean.runtimeType);
+}
